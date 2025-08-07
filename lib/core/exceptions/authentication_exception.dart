e9f4c45e6776a58cee40fd2d29afdaff597c7001
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-08-07 19:59:21 +0200

    :technologist: Enhance AuthenticationException with error type and improved message formatting

--- lib/core/exceptions/authentication_exception.dart
+++ lib/core/exceptions/authentication_exception.dart
@@ -1,11 +1,31 @@
 /// An exception that is thrown when an authentication error occurs.
 class AuthenticationException implements Exception {
   /// Creates an instance of [AuthenticationException] with the provided [message].
-  AuthenticationException(this.message);
+  AuthenticationException(
+    this.message, {
+    this.type = AuthenticationErrorType.authenticationFailed,
+  });
 
   /// The error message associated with the exception.
-  final String message;
+  final String? message;
+
+  /// The type of authentication error that occurred.
+  ///
+  final AuthenticationErrorType type;
 
   @override
-  String toString() => "AuthenticationException: $message";
+  String toString() => "AuthenticationException: $message (Type: $type)";
+}
+
+// TODO(everyone): Add more specific error types if needed
+/// An enum representing the different types of authentication errors.
+enum AuthenticationErrorType {
+  /// Indicates that the user cancelled the authentication process.
+  userCancelled,
+
+  /// Indicates that the authentication process failed due to an error.
+  authenticationFailed,
+
+  /// Indicates that the authentication process was successful.
+  authenticationSuccessful,
 }
