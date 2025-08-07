4d3638ca31f22b3b1d329f2fd5cb485c030151f7
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-08-07 18:47:48 +0200

    :technologist: Improve error handling in OAuth login process with custom error messages

--- lib/ui/login/oauth/login_oauth_view_model.dart
+++ lib/ui/login/oauth/login_oauth_view_model.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:gitdone/core/exceptions/authentication_exception.dart";
+import "package:gitdone/core/exceptions/oauth_request_exception.dart";
 import "package:gitdone/core/models/github_oauth_model.dart";
+import "package:gitdone/core/utils/logger.dart";
 import "package:gitdone/core/utils/navigation.dart";
 import "package:gitdone/ui/main_screen.dart";
 
@@ -10,24 +14,86 @@ class LoginGithubViewModel extends ChangeNotifier {
     : _githubAuth = GitHubAuth(infoCallback);
   final GitHubAuth _githubAuth;
 
+  static const String _classId =
+      "com.GitDone.gitdone.ui.login.oauth.login_oauth_view_model";
+
   /// Callback function to show the user an informational message.
   Function(String) infoCallback;
+
+  bool _errorOccurred = false;
+
+  /// Returns whether an error occurred during the login process.
+  bool get errorOccurred => _errorOccurred;
+
+  /// The error message to be displayed if an error occurred.
+  String errorMessage = "";
 
   /// Starts the login process and returns the user code.
   ///
   /// Returns the `userCode` to be displayed to the user.
   Future<void> startLogin() async {
-    final String userCode = await _githubAuth.authenticate();
+    final String userCode = await _githubAuth.authenticate().catchError((
+      final error,
+      final stackTrace,
+    ) {
+      _handleError(error);
+      return "";
+    });
+
     if (userCode.isEmpty) {
       infoCallback("Login failed. Please try again.");
+      _handleError(userCode);
+      notifyListeners();
       return;
     }
-    final bool loginResult = await _githubAuth.completeLogin(userCode);
+    final bool loginResult = await _githubAuth
+        .completeLogin(userCode)
+        .catchError((final error, final stackTrace) {
+          _handleError(error);
+          return false;
+        });
 
     if (!loginResult) {
       infoCallback("Login failed. Please try again.");
+      _errorOccurred = true;
+      notifyListeners();
       return;
     }
     Navigation.navigate(const MainScreen());
+  }
+
+  /// Returns the current status of the login process.
+  String get status {
+    if (_githubAuth.inLoginProcess) {
+      return "In Progress";
+    } else if (_githubAuth.isAuthenticated) {
+      return "Authenticated";
+    } else {
+      return "Not Started";
+    }
+  }
+
+  void _handleError(final Object error) {
+    _errorOccurred = true;
+
+    if (error.runtimeType == PlatformException) {
+      errorMessage = "It seems that you have cancelled the login process.";
+    } else if (error.runtimeType == OAuthRequestException) {
+      errorMessage =
+          "It seems that the Server does not respond. "
+          "Please try again later.";
+    } else if (error.runtimeType == AuthenticationException) {
+      errorMessage =
+          "We did not receive a valid response from the server. "
+          "Please try again later.";
+    } else {
+      errorMessage = "An unexpected error occurred.";
+    }
+    notifyListeners();
+    Logger.log(
+      "Error in LoginGithubViewModel: $error",
+      _classId,
+      LogLevel.shout,
+    );
   }
 }
