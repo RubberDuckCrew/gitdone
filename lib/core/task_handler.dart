18907208d53cd1be9932c29d35046cc1d980ecb9
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-08-09 08:56:16 +0200

    :adhesive_bandage: Prevent duplicate tasks in local list when saving

--- lib/core/task_handler.dart
+++ lib/core/task_handler.dart
@@ -79,7 +79,14 @@ class TaskHandler extends ChangeNotifier {
   Future<Task> saveTask(final Task task) async {
     try {
       final Task createdTask = await task.saveRemote();
-      _tasks.add(createdTask);
+
+      // Add the created task to the local list of tasks if it does not already exist
+      // This prevents duplicates in the local task list.
+      // Reference: https://github.com/RubberDuckCrew/gitdone/issues/179
+      if (!_tasks.any((final t) => t.issueNumber == createdTask.issueNumber)) {
+        _tasks.add(createdTask);
+      }
+
       notifyListeners();
       return createdTask;
     } on Exception catch (e) {
