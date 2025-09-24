89429d577233cb71a2e943f3b846b9257a568137
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-09-24 20:52:24 +0200

    ♻️ Improve DownloadRelease code

--- docs/src/download/DownloadRelease.vue
+++ docs/src/download/DownloadRelease.vue
@@ -5,18 +5,20 @@
     <template v-else-if="status === 'success'">
         <p>
             <VPButton
-                text="Download latest stable release"
+                :text="`Download latest stable release (${releaseInfo.name})`"
                 theme="brand"
-                v-if="releaseUrl"
-                :href="releaseUrl" />
+                v-if="releaseInfo"
+                :href="releaseInfo.url" />
             <VPButton text="No stable release available" theme="alt" v-else />
         </p>
         <p>
             <VPButton
-                text="Download latest pre-release"
+                :text="`Download latest pre-release (${preReleaseInfo.name})`"
                 theme="brand"
-                v-if="preReleaseUrl"
-                :href="preReleaseUrl" />
+                v-if="
+                    preReleaseInfo && preReleaseInfo.name !== releaseInfo?.name
+                "
+                :href="preReleaseInfo.url" />
             <VPButton text="No pre-release available" theme="alt" v-else />
         </p>
     </template>
@@ -37,52 +39,47 @@ import { ref, onMounted } from "vue";
 import VPButton from "vitepress/dist/client/theme-default/components/VPButton.vue";
 
 const status = ref<DownloadStatus>("loading");
-const preReleaseUrl = ref("");
-const releaseUrl = ref("");
+const preReleaseInfo = ref<ReleaseInfo | null>(null);
+const releaseInfo = ref<ReleaseInfo | null>(null);
 
 type DownloadStatus = "loading" | "success" | "error";
+type Release = {
+    tag_name: string;
+    prerelease: boolean;
+    assets: Asset[];
+    published_at: string;
+};
+type Asset = {
+    name: string;
+    browser_download_url: string;
+};
+type ReleaseInfo = {
+    name: string;
+    url: string;
+};
 
 onMounted(() => {
     fetch("https://api.github.com/repos/RubberDuckCrew/gitdone/releases")
         .then((res) => res.json())
         .then((data) => {
             data.sort(
-                (a: any, b: any) =>
+                (a: Release, b: Release) =>
                     new Date(b.published_at).getTime() -
                     new Date(a.published_at).getTime()
             );
             const latestRelease = data.find(
-                (release: any) => !release.prerelease
+                (release: Release) => !release.prerelease
             );
             const latestPreRelease = data.find(
-                (release: any) => release.prerelease
+                (release: Release) =>
+                    release.prerelease &&
+                    new Date(release.published_at) >
+                        new Date(latestRelease?.published_at ?? 0)
             );
-            if (
-                latestPreRelease &&
-                latestPreRelease.assets &&
-                latestPreRelease.assets.length > 0
-            ) {
-                const apk = latestPreRelease.assets.find((a: any) =>
-                    a.name.endsWith(".apk")
-                );
-                if (apk) {
-                    preReleaseUrl.value = apk.browser_download_url;
-                    status.value = "success";
-                }
-            }
-            if (
-                latestRelease &&
-                latestRelease.assets &&
-                latestRelease.assets.length > 0
-            ) {
-                const apk = latestRelease.assets.find((a: any) =>
-                    a.name.endsWith(".apk")
-                );
-                if (apk) {
-                    releaseUrl.value = apk.browser_download_url;
-                    status.value = "success";
-                }
-            }
+            preReleaseInfo.value = extractReleaseInfo(latestPreRelease);
+            releaseInfo.value = extractReleaseInfo(latestRelease);
+            status.value =
+                preReleaseInfo.value || releaseInfo.value ? "success" : "error";
             if (status.value !== "success") {
                 status.value = "error";
             }
@@ -91,4 +88,19 @@ onMounted(() => {
             status.value = "error";
         });
 });
+
+function extractReleaseInfo(release: Release | undefined): ReleaseInfo | null {
+    if (release && release.assets && release.assets.length > 0) {
+        const apk = release.assets.find((asset: Asset) =>
+            asset.name.endsWith(".apk")
+        );
+        if (apk) {
+            return {
+                name: release.tag_name,
+                url: apk.browser_download_url,
+            };
+        }
+    }
+    return null;
+}
 </script>
